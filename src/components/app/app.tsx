75421b39e52e8b145b8519d2a1392c85e3b918fe
VslavV <um.w@mail.ru>
2024-11-26 19:33:59 +0500

    Создает компонент главной страницы,  карточки предложения. Создает компонент App для отрисовки главной страницы. Редактирует файл index.tsx , передает компоненту App пропс

---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  offerCardsCount: number;
+};
+
+function App({ offerCardsCount }: AppScreenProps): JSX.Element {
+  return <MainScreen offerCardsCount={offerCardsCount} />;
+}
+
+export default App;
