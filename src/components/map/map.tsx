93d34ba27d086a8044692cd2428a736c27266380
VslavV <um.w@mail.ru>
2024-11-26 23:23:12 +0500

    выделение переиспользуемых компонентов

---
+++ src/components/map/map.tsx
@@ -0,0 +1,11 @@
+type MapProps ={
+  className: string;
+};
+
+function Map ({className}: MapProps):JSX.Element {
+  return (
+    <section className={`${className} map`}></section>
+  );
+}
+
+export default Map;
