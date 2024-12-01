cec85f906b5a7999c436781dd79638a702498881
VslavV <um.w@mail.ru>
2024-12-01 17:56:18 +0500

    используя props, добавляет  для главной страницы приложения изменяемые данные — это будет количество предложений аренды.

--- src/pages/main-screen/main-screen.tsx
+++ src/pages/main-screen/main-screen.tsx
@@ -27,7 +27,7 @@ function MainScreen({ offerCardsCount }: MainScreenProps): JSX.Element {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
+              <b className="places__found">{offerCardsCount} places to stay in Amsterdam</b>
               <Sort/>
               <div className="cities__places-list places__list tabs__content">
                 {offerCardsList};
