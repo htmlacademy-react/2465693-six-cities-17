36209cd0042825146337fdabc8c5842665ee0ec9
VslavV <um.w@mail.ru>
2024-12-01 23:17:06 +0500

    Смена имени названия компонентов *-screen на *-page.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,9 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
-import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
-import OfferScreen from '../../pages/offer-screen/offer-screen';
-import LoginScreen from '../../pages/login-screen/login-screen';
-import MainScreen from '../../pages/main-screen/main-screen';
+import FavoritesScreen from '../../pages/favorites-page/favorites-page.tsx';
+import NotFoundScreen from '../../pages/not-found-page/not-found-page.tsx';
+import OfferScreen from '../../pages/offer-page/offer-page.tsx';
+import LoginScreen from '../../pages/login-page/login-page.tsx';
+import MainScreen from '../../pages/main-page/main-page.tsx';
 import PrivateRoute from '../private-route/private-route.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import { RoutePath, AuthorizationStatus } from '../../const.ts';
