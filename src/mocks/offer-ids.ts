40ce24d9c60b9b60a513b9044943168bd56bdd55
VslavV <um.w@mail.ru>
2024-12-05 18:58:39 +0500

    Добаялет моки для карточки предложения, комментариев, список предложений неподалеку

---
+++ src/mocks/offer-ids.ts
@@ -0,0 +1,189 @@
+import { RentalOffer } from '../types/offer';
+
+export const offerIds: RentalOffer[] = [
+  {
+    'id': 'b8c329f9-619c-4ee9-a537-4a88d58c56d8',
+    'title': 'The Pondhouse - A Magical Place',
+    'description': 'A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.',
+    'type': 'apartment',
+    'price': 469,
+    'images': [
+      'https://16.design.htmlacademy.pro/static/hotel/11.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/14.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/13.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/5.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/1.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/19.jpg'
+    ],
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'goods': [
+      'Breakfast',
+      'Washing machine',
+      'Dishwasher',
+      'Washer',
+      'Fridge'
+    ],
+    'host': {
+      'isPro': true,
+      'name': 'Angelina',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    'isPremium': false,
+    'isFavorite': false,
+    'rating': 2.4,
+    'bedrooms': 4,
+    'maxAdults': 5
+  },
+  {
+    'id': 'b8c329f9-619c-4ee9-a537-4a88d58c56d8',
+    'title': 'The Pondhouse - A Magical Place',
+    'description': 'A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.',
+    'type': 'apartment',
+    'price': 469,
+    'images': [
+      'https://16.design.htmlacademy.pro/static/hotel/11.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/14.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/13.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/5.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/1.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/19.jpg'
+    ],
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'goods': [
+      'Breakfast',
+      'Washing machine',
+      'Dishwasher',
+      'Washer',
+      'Fridge'
+    ],
+    'host': {
+      'isPro': true,
+      'name': 'Angelina',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    'isPremium': false,
+    'isFavorite': false,
+    'rating': 2.4,
+    'bedrooms': 4,
+    'maxAdults': 5
+  },
+  {
+    'id': 'eae6718c-1204-45a3-93ae-51c8841bc33e',
+    'title': 'Wood and stone place',
+    'description': 'A new spacious villa, one floor. All commodities, jacuzzi and beautiful scenery. Ideal for families or friends.',
+    'type': 'house',
+    'price': 534,
+    'images': [
+      'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/7.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/6.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/10.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/20.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/2.jpg'
+    ],
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.335499,
+      'zoom': 16
+    },
+    'goods': [
+      'Dishwasher',
+      'Air conditioning',
+      'Washer',
+      'Baby seat',
+      'Washing machine',
+      'Breakfast'
+    ],
+    'host': {
+      'isPro': true,
+      'name': 'Angelina',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    'isPremium': false,
+    'isFavorite': false,
+    'rating': 1.7,
+    'bedrooms': 4,
+    'maxAdults': 4
+  },
+  {
+    'id': '8867858c-95d1-47fd-8619-f8dab016b8d7',
+    'title': 'Penthouse, 4-5 rooms + 5 balconies',
+    'description': 'Relax, rejuvenate and unplug in this ultimate rustic getaway experience in the country. In our beautiful screened Pondhouse, you can gaze at the stars and listen to the sounds of nature from your cozy warm bed.',
+    'type': 'room',
+    'price': 182,
+    'images': [
+      'https://16.design.htmlacademy.pro/static/hotel/1.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/3.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/4.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/10.jpg',
+      'https://16.design.htmlacademy.pro/static/hotel/12.jpg'
+    ],
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'goods': [
+      'Laptop friendly workspace',
+      'Washer',
+      'Breakfast',
+      'Cable TV',
+      'Dishwasher',
+      'Washing machine',
+      'Coffee machine',
+      'Towels',
+      'Heating'
+    ],
+    'host': {
+      'isPro': true,
+      'name': 'Angelina',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
+    },
+    'isPremium': false,
+    'isFavorite': false,
+    'rating': 1.4,
+    'bedrooms': 1,
+    'maxAdults': 2
+  }
+];
---
+++ src/mocks/offers.ts
@@ -0,0 +1,165 @@
+import { RentalOffer } from '../types/offer';
+
+export const offers:RentalOffer[] = [
+  {
+    'id': '4dca75c7-04d9-44ae-ada6-1a9d4e075fda',
+    'title': 'Amazing and Extremely Central Flat',
+    'type': 'apartment',
+    'price': 161,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.868610000000004,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.7
+  },
+  {
+    'id': 'b8c329f9-619c-4ee9-a537-4a88d58c56d8',
+    'title': 'The Pondhouse - A Magical Place',
+    'type': 'apartment',
+    'price': 469,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/19.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 2.4
+  },
+  {
+    'id': 'eae6718c-1204-45a3-93ae-51c8841bc33e',
+    'title': 'Wood and stone place',
+    'type': 'house',
+    'price': 534,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/17.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.335499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.7
+  },
+  {
+    'id': '8867858c-95d1-47fd-8619-f8dab016b8d7',
+    'title': 'Penthouse, 4-5 rooms + 5 balconies',
+    'type': 'room',
+    'price': 182,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/13.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.4
+  },
+  {
+    'id': '60281559-9180-4699-95b1-baff7fab22eb',
+    'title': 'Beautiful & luxurious apartment at great location',
+    'type': 'apartment',
+    'price': 134,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/10.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.87561,
+      'longitude': 2.375499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 3.8
+  },
+  {
+    'id': '3e37a72a-d721-4e1c-b488-1413390449c8',
+    'title': 'Canal View Prinsengracht',
+    'type': 'hotel',
+    'price': 226,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/13.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.87961000000001,
+      'longitude': 2.353499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4.9
+  },
+  {
+    'id': '015cf407-aebc-45bf-872e-d1bc6e15d5ac',
+    'title': 'The Pondhouse - A Magical Place',
+    'type': 'hotel',
+    'price': 500,
+    'previewImage': 'https://16.design.htmlacademy.pro/static/hotel/3.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.364499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.9
+  }
+];
