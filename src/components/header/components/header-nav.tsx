4d40d03e74a1524ef926c99da1dbdeca4239366d
VslavV <um.w@mail.ru>
2025-01-09 23:55:15 +0500

    исправляет логику рендеринга на странице логин

--- src/components/header/components/header-nav.tsx
+++ src/components/header/components/header-nav.tsx
@@ -7,40 +7,46 @@ function HeaderNav (): JSX.Element {
   const dispatch = useAppDispatch();
   const {userInfo} = useAppSelector((state) =>state);
 
+  const handleSignOutClick = () =>{
+    dispatch(logoutAction());
+  };
+
   return (
     <nav className="header__nav">
       <ul className="header__nav-list">
         <li className="header__nav-item user">
-          <Link
-            className="header__nav-link header__nav-link--profile"
-            to={RoutePath.Favorites}
-          >
-            <div className="header__avatar-wrapper user__avatar-wrapper"/>
-            <span className="header__user-name user__name">
-              {userInfo?.email}
-            </span>
-            {userInfo && <span className="header__favorite-count">3</span>}
-          </Link>
+          {userInfo ?
+            <Link
+              className="header__nav-link header__nav-link--profile"
+              to={RoutePath.Favorites}
+            >
+              <div className="header__avatar-wrapper user__avatar-wrapper"/>
+              <span className="header__user-name user__name">
+                {userInfo?.email}
+              </span>
+              {userInfo && <span className="header__favorite-count">3</span>}
+            </Link>
+            :
+            <Link
+              className="header__nav-link header__nav-link--profile"
+              to={RoutePath.Login}
+            >
+              <div className="header__avatar-wrapper user__avatar-wrapper"/>
+              <span className="header__login">Sign in</span>
+            </Link>}
         </li>
         <li className="header__nav-item">
           {
-            userInfo ?
+            userInfo &&
               <a
                 className="header__nav-link"
                 onClick={(evt) => {
                   evt.preventDefault();
-                  dispatch(logoutAction());
+                  handleSignOutClick();
                 }}
               >
                 <span className="header__signout">Sign out</span>
               </a>
-              :
-              <Link
-                className="header__nav-link"
-                to={RoutePath.Login}
-              >
-                <span className="header__login">Sign in</span>
-              </Link>
           }
         </li>
       </ul>
